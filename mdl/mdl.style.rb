16d96ef204f5eb6287e4bf90e05cc8dcc89a7267
Matt Miller <mattmiller1@gmail.com>
2023-05-12 18:02:28 -0400

    mdl: exclude rule for surrounding code by black lines and allow nesting of the same header

--- mdl/mdl.style.rb
+++ mdl/mdl.style.rb
@@ -1,4 +1,6 @@
 all
+exclude_rule 'MD031'
 rule 'MD007', :indent => 4
 rule 'MD010', :ignore_code_blocks => true
 rule 'MD013', :ignore_code_blocks => true
+rule 'MD024', :allow_different_nesting => true
