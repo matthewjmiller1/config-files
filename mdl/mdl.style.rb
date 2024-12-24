bd8fdcbd052a05af7d1ef8cc43d791238f11f0b6
Matt Miller <mattmiller1@gmail.com>
2024-12-24 16:08:05 -0500

    Update mdl lint to not check line length for tables

--- mdl/mdl.style.rb
+++ mdl/mdl.style.rb
@@ -4,5 +4,5 @@ exclude_rule 'MD031'
 exclude_rule 'MD033'
 rule 'MD007', :indent => 4
 rule 'MD010', :ignore_code_blocks => true
-rule 'MD013', :ignore_code_blocks => true
+rule 'MD013', :ignore_code_blocks => true, :tables => false
 rule 'MD024', :allow_different_nesting => true
